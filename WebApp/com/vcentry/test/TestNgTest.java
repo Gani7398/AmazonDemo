77d723b52db79d3c34efb1137568e1ca6e423264
Gani7398 <ganeshgani7398@gmail.com>
2022-09-03 13:14:29 +0530

    first commit

---
+++ WebApp/com/vcentry/test/TestNgTest.java
@@ -0,0 +1,16 @@
+package com.vcentry.test;
+
+public class TestNgTest {
+	
+	// Added by Ganesh - Tester 1
+	@Test
+	public void addmethod() {
+		
+		System.out.println("Test");
+		
+	}
+	
+	
+	
+
+}
